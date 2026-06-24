c06c5aa9c82da05b053eba2b8af9f6c0381030ac
Tanmoy Panja <tanmoypanja234@gmail.com>
2026-06-24 06:41:28 +0530

    Time: 0 ms (100%), Space: 47.1 MB (81.25%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expected = n*(n+1)/2;
+        int actual = 0;
+
+        for(int i : nums)
+            actual += i;
+        
+        return expected - actual;
+    }
+}
