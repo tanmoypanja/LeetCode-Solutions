8adf323aa6cabab584cbcb1b5c9943d9f347855e
Tanmoy Panja <tanmoypanja234@gmail.com>
2026-06-29 08:37:08 +0530

    Time: 25 ms (5.04%), Space: 42.4 MB (77.82%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int mySqrt(int x) {
+        int ans = 0;
+        for(long i = 1; i <= x; i++){
+            if (i*i <= x)
+                ans = (int)i;
+            else
+                break;
+        }
+        return ans;
+    }
+}
