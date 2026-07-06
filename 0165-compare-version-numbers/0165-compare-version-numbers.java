56d7ec62a6f94dbdcfae3ee55238289239ba403a
Tanmoy Panja <tanmoypanja234@gmail.com>
2026-07-06 07:38:59 +0530

    Time: 0 ms (100%), Space: 42.6 MB (87.49%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,20 +1,39 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] v1 = version1.split("\\.");
-        String[] v2 = version2.split("\\.");
-        int n = Math.max(v1.length, v2.length);
-        for(int i = 0; i < n; i++){
-            int num1 = (i < v1.length)
-                    ? Integer.parseInt(v1[i])
-                    : 0;
-            int num2 = (i < v2.length)
-                    ? Integer.parseInt(v2[i])
-                    : 0;
-            if(num1 > num2)
-                return 1;
-            if(num1 < num2)
+        int i = 0, j = 0;
+        int n1 = version1.length();
+        int n2 = version2.length();
+
+        // Loop until both version strings are completely traversed
+        while (i < n1 || j < n2) {
+            int num1 = 0;
+            int num2 = 0;
+            
+            // Extract the numerical value of the current revision in version1
+            while (i < n1 && version1.charAt(i) != '.') {
+                num1 = num1 * 10 + (version1.charAt(i) - '0');
+                i++;
+            }
+            
+            // Extract the numerical value of the current revision in version2
+            while (j < n2 && version2.charAt(j) != '.') {
+                num2 = num2 * 10 + (version2.charAt(j) - '0');
+                j++;
+            }
+            
+            // Compare the revision values
+            if (num1 < num2) {
                 return -1;
+            } else if (num1 > num2) {
+                return 1;
+            }
+            
+            // Move past the dot '.' character for the next iteration
+            i++;
+            j++;
         }
+        
+        // If all revisions are equal, return 0
         return 0;
     }
 }
